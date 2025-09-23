cc5c26fa16f1c48dc4d741090a531162e40f595b
Sala213 <Sala213@ezse.pl>
2025-09-23 09:52:19 +0200

    End of Lesson-M.O.

---
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+//Napisz program który wczyta liczbę od użytkownika i ją wyświetli na kontroli
+void task1()
+{
+   //wczytanie liczby
+      //deklaracja zmennych         1
+     //pobranie liczby          
+        //informcja co chcemy       2
+        //oczekiwanie na liczbę     3
+
+    //wyświetlenie na konsoli       4
+
+    //1
+    int numberFromUser; //number_from_user
+    //2
+    std::cout << "Podaj liczbę całkowtą;\n";
+    //3
+    std::cin >> numberFromUser;
+    //4
+    std::cout << " podałeś " << numberFromUser << "\n";
+}
+
+int main()
+{
+    task1();
+}
+
+//Program obliczający średnią arytmetyczną dwóch liczb
+void task2()
+{
+
+
+}
+
+
+int main()
+{
+    task2();
+}
+/* 
+
+ Zmienna - pewien obszar w pamięci operacyjnej, w której można 
+ w danej chwili przechować tylko jedną daną.
+
+  instrukcja daklaracji zmiennej:
+  typ_zmiennej nazwa_zmiennej;
+   
+  Typ_zmiennej - wielkość obszaru pamięci, interpretacja ciągu bitów
+
+  int - 4 bajtowa liczba całkowita ze znakiem <-2 147 483 648, 2 147 483 647>
+  
+  
+  
+  
+ 
+
+
+
+
+
+
+
+
+
+*/

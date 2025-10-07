9632e4d1bede2d5cc23d5502cb4220ecbc530232
Sala213 <Sala213@ezse.pl>
2025-10-07 09:55:32 +0200

    koniec zadań

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -1,5 +1,17 @@
 ﻿#include <iostream>
 
+/*
+*.
+*. 
+*. 
+*. 
+*.
+*. Program obliczający objętość kuli o promieniu r
+*. Program obliczający pole trapezu o podstawach a i b oraz wysokości h
+*. Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+*. Stwórz program, który pobiera od użytkownika długość w metrach i przelicza ją na centymetry i milimetry, wyświetlając wynik.
+*. Napisz program, który konwertuje wprowadzoną kwotę w jednej walucie na inną, korzystając z określonego kursu wymiany.
+*/
 //Napisz program który wczyta liczbę od użytkownika i ją wyświetli na kontroli
 void task1()
 {
@@ -60,18 +72,79 @@ void task3()
 
 
 
-
-int main()
+//Program obliczający objętość stożka.
+void task4()
 {
-   
-    task3();
+    int r, h;
+
+    std::cout << "podaj długość promienia podstawy ";
+    std::cin >> r; 
+
+    std::cout << "podaj długość promienia wysokości ";
+    std::cin >> h;
+    
+    float area;
+
+    area =3.14* r * r * h/3;
+
+    std::cout << "wynik" << area << "\n";
+}
+//Program obliczający pole koła.
+void task5()
+{
+    int r; 
+
+    std::cout << "podaj długość promienia koła ";
+    std::cin >> r;
+
+    float area;
+
+    area = 3.14 * r * r;
+
+    std::cout << "wynik" << area << "\n"; 
+}
+//Program obliczający wartość wyrażenia a^2 + b^2
+void task6()
+{
+    int a, b;
+    
+    std::cout << "podaj wartość wyrażenia a^2";
+    std::cin >> a;
+
+    std::cout << "podaj wartość wyrażenia b^2";
+    std::cin >> b;
+
+    int area;
+
+    area = a ^ 2 + b ^ 2;
+
+    std::cout << "wynik" << area << "\n";
+
+}
+
+//Program obliczający pole trójkąta o podstawie b i wysokości h
+void task7()
+{
+    int b, h;
+    std::cout << "podaj długość podstawy b";
+    std::cin >> b;
+
+    std::cout << "podaj długość wysokość h"; 
+    std::cin >> h;
+    
+    float area;
+
+    area = b * h ^2;
+      
+    std::cout << "wynik" << area << "\n";
 }
 
 
-
-
-
-
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    task7();
+}
 
 
 /* 

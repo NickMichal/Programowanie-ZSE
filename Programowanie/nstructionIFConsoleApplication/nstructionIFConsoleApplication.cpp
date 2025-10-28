a0404b8dca16c63ed52c6ea56bf79d251b9f6f86
Sala213 <Sala213@ezse.pl>
2025-10-28 08:26:17 +0100

    koniec

--- Programowanie/nstructionIFConsoleApplication/nstructionIFConsoleApplication.cpp
+++ Programowanie/nstructionIFConsoleApplication/nstructionIFConsoleApplication.cpp
@@ -1,86 +1,73 @@
-﻿//	instructionIFConsoleApplication.cpp : This file contains the 'main' function.Program execution begins and ends there.
-//
+﻿#include <iostream>
 
+/*
 
+DRY - don't repeat yourself
+KISS - kept it simple stiupid
 
+Operatry warunkowe:
+>		- większy
+<		- mniejszy
+>=		- wiekszy bądź równy
+<=		- mniejszy bądź równy
+==		- równy
+!=		- różny
 
-#include <iostream>
+Operatory logiczne:
+&&	-	AND
+||	-	OR
+!	-   NOT
 
+a	b		a && b		a || b		!a
+F	F		  F			   F		 T
+F	T		  F			   T		 T
+T	F		  F			   T		 F
+T	T	      T			   T		 F
 
-	//Napisz program, który wyświetli informacje czy liczba jest dodatnia czy nie.
-	void task1()
-	{
-		int number;
-		std::cout << "Podaj liczbę całkowitą:\n";
-		std::cin >> number;
+a && b || c && d
 
-		if (number > 0)
-			std::cout << "Podana liczba jest dodatnia\n";
+*/
 
-		if (number < 0)
-			std::cout << "Podana liczba jest ujemna\n";
+//Napisz program, który wyświetli informacje czy liczba jest dodatnia czy nie.
+@@ -47,7 +60,7 @@
+	if (reminderOfDivision == 0)
+		std::cout << "Podana liczba jest parzysta\n";
 
-		if (number < 0)
-			std::cout << "Podana liczba nie jest dodatnia ani ujemna\n";
-		
-		std::cout << "Koniec programu\n";
-	}
+	if (reminderOfDivision != 0) // == 1 
+	if (reminderOfDivision == 0) // == 1 
+		std::cout << "Podana liczba jest nieparzysta\n";
 
+	//--------------------------------------------------------
+@@ -85,6 +98,26 @@
+			std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	else
+		std::cout << "Liczba nie jest w zakresie <1;10)\n";
 
+	//wersja 3
+	if (number >= 1 && number < 10 )
+		std::cout << "Liczba jest w zakresie <1;10)\n";
+	else
+		std::cout << "Liczba nie jest w zakresie <1;10)\n";
 
+	//wersja 4
+	if (number < 1 || number >= 10)
+		std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	else
+		std::cout << "Liczba jest w zakresie <1;10)\n";
 
-	
-	//Napisz program, który wyświetli informacje czy liczba jest parzysta czy nieparzysta.
-	void task2()
-	{
-		int number;
-		 std::cout << "Podaj liczbę całkowitą:\n";
-		 std::cin >> number;
-
-		 int remiderOfDivisoin;
-		 remiderOfDivisoin = number % 2;
-
-		  if (remiderOfDivisoin == 0)
-		  std::cout << "Podana liczba jest parzysta\n";
-
-		  if (remiderOfDivisoin != 0) // == 1
-		  std::cout << "Podana liczba jest nieparzysta\n";
-
-		//------------------------------------------------------------
-		  
-		  if (remiderOfDivisoin == 0)
-			  std::cout << "Podana liczba jest parzysta\n";
-		  else
-			  std::cout << "Podana liczba jest nieparzysta\n";
-	}
-	 	 
-	//Napisz program, który wyświetli informacje czy liczba jest z zakresu <1 ; 10).
-	void task3()
-	{
-
-		int number;
-		std::cout << "Podaj liczbę całkowitą:\n";
-		std::cin >> number;
-
-	}
-
-	int main()		 		 
-	{
+	//wersja 5
+	if (!(number >= 1 && number < 10))
+		std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	else
+		std::cout << "Liczba jest w zakresie <1;10)\n";
 		
 
-		setlocale(LC_CTYPE, "polish");
+}
 
-		task3();
-	}
+int main()
 
+{
+	setlocale(LC_CTYPE, "polish");
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	task4();
+}

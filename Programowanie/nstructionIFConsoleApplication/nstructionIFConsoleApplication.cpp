f313486480f1108a517d16da4d6030dba8563f5c
Sala213 <Sala213@ezse.pl>
2025-11-03 10:39:22 +0100

    xaazs

--- Programowanie/nstructionIFConsoleApplication/nstructionIFConsoleApplication.cpp
+++ Programowanie/nstructionIFConsoleApplication/nstructionIFConsoleApplication.cpp
@@ -29,22 +29,79 @@ a && b || c && d
 */
 
 //Napisz program, który wyświetli informacje czy liczba jest dodatnia czy nie.
-@@ -47,7 +60,7 @@
+void task5()
+{
+	int number;
+	std::cout << "Podaj liczbę całkowitą:\n";
+	std::cin >> number;
+
+	if (number > 0)
+		std::cout << "Podana liczba jest dodatnia\n";
+
+	if (number < 0)
+		std::cout << "Podana liczba jest ujemna\n";
+
+	if (number == 0)
+		std::cout << "Podana liczba nie jest dodatnia ani ujemna\n";
+
+	std::cout << "Koniec programu\n";
+}
+
+//Napisz program, który wyświetli informacje czy liczba jest parzysta czy nieparzysta.
+void task6()
+{
+	int number;
+	std::cout << "Podaj liczbę całkowitą:\n";
+	std::cin >> number;
+
+	int reminderOfDivision;
+	reminderOfDivision = number % 2;
+
 	if (reminderOfDivision == 0)
 		std::cout << "Podana liczba jest parzysta\n";
 
-	if (reminderOfDivision != 0) // == 1 
 	if (reminderOfDivision == 0) // == 1 
 		std::cout << "Podana liczba jest nieparzysta\n";
 
 	//--------------------------------------------------------
-@@ -85,6 +98,26 @@
+	if (reminderOfDivision == 0)
+		std::cout << "Podana liczba jest parzysta\n";
+	else
+		std::cout << "Podana liczba jest nieparzysta\n";
+}
+
+//Napisz program, który wyświetli informacje czy liczba jest z zakresu <1 ; 10).
+
+void task7()
+{
+	int number;
+	std::cout << "Podaj liczbę całkowitą:\n";
+	std::cin >> number;
+
+	//wersja 1
+	if (number >= 1)
+	{
+		if (number < 10)
+			std::cout << "Liczba jest w zakresie <1;10)\n";
+		else
+			std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	}
+	else
+	{
+		std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	}
+
+	//wersja 2
+	if (number >= 1)
+		if (number < 10)
+			std::cout << "Liczba jest w zakresie <1;10)\n";
+		else
 			std::cout << "Liczba nie jest w zakresie <1;10)\n";
 	else
 		std::cout << "Liczba nie jest w zakresie <1;10)\n";
 
 	//wersja 3
-	if (number >= 1 && number < 10 )
+	if (number >= 1 && number < 10)
 		std::cout << "Liczba jest w zakresie <1;10)\n";
 	else
 		std::cout << "Liczba nie jest w zakresie <1;10)\n";
@@ -60,14 +117,32 @@ a && b || c && d
 		std::cout << "Liczba nie jest w zakresie <1;10)\n";
 	else
 		std::cout << "Liczba jest w zakresie <1;10)\n";
-		
-
 }
 
-int main()
-
+//Napisz program, który wykona dzielenie dwóch liczb
+void task8()
 {
+	int dividend, divisor;
+	std::cout << "Podaj dzielnik:\n";
+	std::cin >> dividend;
+	std::cout << "Podaj dzielną:\n";
+	std::cin >> divisor;
+
+	if (divisor != 0)
+	{
+		int quoitent = dividend / divisor;
+		std::cout << "Wynik dzielenia " << quoitent << "\n";
+	}
+	else
+		std::cout << "Dzzielenie przez zero!!!\n";
+}
+
+
+int main()
+{
+
+
 	setlocale(LC_CTYPE, "polish");
 
-	task4();
+	task8();
 }
